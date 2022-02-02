39873b83fcecc8bbf2a0e8f4701a701e30f4ddaf
Nights_Wolf <dawinavi@gmail.com>
2022-02-02 01:26:55 +0100

    little changes in section margin

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import zakopane from "./trips";
 import Footer from "./Footer";
 
 function App() {
-  const travel = zakopane.map(trip => {
-    return <Card 
-    img={trip.img}
-    destiny={trip.destiny}
-    date={trip.date}  
-    description={trip.description}/>
+    const travel = zakopane.map(trip => {
+      return <Card 
+      img={trip.img}
+      destiny={trip.destiny}
+      date={trip.date}  
+      description={trip.description}/>
   })
   return(
     <div>
     <Header />
     <section className="section--card">
-    {travel}
+    {zakopane.length >= 1 ? travel: <p></p>}
+    <button className="right-button">&#707;</button>
+    <button className="left-button">&#706;</button>
     </section>
     <Footer />
     </div>
