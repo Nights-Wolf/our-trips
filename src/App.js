206a59ab85d2defe9e06f7a05a81e8ddb3f221f5
Nights_Wolf <dawinavi@gmail.com>
2022-02-05 01:36:27 +0100

    newa card added. Cleaning code in trips and App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import Header from "./Header";
 import Card from "./Card";
-import zakopane from "./trips";
+import trips from "./trips";
 import Footer from "./Footer";
 import react from "react";
 
 function App() {
-    const [card, swapCard] = react.useState(zakopane.map(trip => {
+    const trip = trips.map(trip => {
       return <Card
       key={trip.id} 
       img={trip.img}
       destiny={trip.destiny}
       date={trip.date}  
       description={trip.description}/>
-  }))
+  })
 
   return(
     <div>
     <Header />
     <section className="section--card">
-    {card}
+    {trip}
     </section>
     <Footer />
     </div>
--- src/trips.js
+++ src/trips.js
@@ -1,25 +1,32 @@
-let zakopane = [
+let trips = [
 {
     id: 1,
     img: "londyn.jpeg", 
     destiny: "England", 
-    date: "11.01.2019-18.09.2019", 
+    date: "11.01.2019 - 18.09.2019", 
     description: "We went to England to visit Kamila's sister. It was a week long trip, in which we seen beutifull village named Stoke Buerne, Towcester (in which wwe had quarters) and London - Museum of Natural History, seen Big Ben (which was covered due to renovation) and Sherlock Holmes Museum." 
 },
 {
     id: 2,
     img: "zakopane.jpg",
     destiny: "Zakopane", 
-    date: "14.01.2022-17.01.2022", 
+    date: "14.01.2022 - 17.01.2022", 
     description: "We visited Zakopane at 14.01.2022. It was a long, 11hrs train trip, but eventually, we arrived at confy sleeproom. First day, we went to see Gubałówka, and bought some souvenirs for grandpa and grandmom days. After that, we head to see ski jumping - our prime goal. Second day - Kamil Stoch's museum, Krupówki, and ski jumping again. Trip was great. Flaws - city communication and long journey."
 },
 {
     id: 3,
     img:"Iceland.jpg",
-    class: "card",
     destiny: "Iceland",
+    date: "Unknown",
+    description: "Still planning..."
+},
+{
+    id: 4,
+    img:"utah_colorado_wyoming.jpg",
+    destiny: "Utah/Kolorado/Wyoming",
     date: "Unknown",
     description: "Still planning..."
 }
 ];
-export default zakopane;
+
+export default trips;
