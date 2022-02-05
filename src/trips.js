e20f3722bd60c2dc72047603775f6451159f6e01
Nights_Wolf <dawinavi@gmail.com>
2022-02-05 01:54:30 +0100

    New cards added

--- src/trips.js
+++ src/trips.js
@@ -1,27 +1,41 @@
 let trips = [
 {
     id: 1,
+    img: "stadion_narodowy.jpg", 
+    destiny: "Warsaw", 
+    date: "12.08.2018 - 14.08.2018", 
+    description: "That was our's first, offical trip. We went to Warsaw because Kamila had tickets for Ed Sheeran's gig on Stadion Narodowy. A day after gig, we went to see Park Łazienkowski." 
+},
+{
+    id: 2,
+    img: "torun.jpg", 
+    destiny: "Toruń", 
+    date: "Multiple times", 
+    description: "We've seen Toruń a couple times. First time we were on simple trip, to see old town. Second time, we went for specific reason - Copernicon, a fantasy fest, which took place at old town."
+},
+{
+    id: 3,
     img: "londyn.jpeg", 
     destiny: "England", 
     date: "11.01.2019 - 18.09.2019", 
     description: "We went to England to visit Kamila's sister. It was a week long trip, in which we seen beutifull village named Stoke Buerne, Towcester (in which wwe had quarters) and London - Museum of Natural History, seen Big Ben (which was covered due to renovation) and Sherlock Holmes Museum." 
 },
 {
-    id: 2,
+    id: 4,
     img: "zakopane.jpg",
     destiny: "Zakopane", 
     date: "14.01.2022 - 17.01.2022", 
     description: "We visited Zakopane at 14.01.2022. It was a long, 11hrs train trip, but eventually, we arrived at confy sleeproom. First day, we went to see Gubałówka, and bought some souvenirs for grandpa and grandmom days. After that, we head to see ski jumping - our prime goal. Second day - Kamil Stoch's museum, Krupówki, and ski jumping again. Trip was great. Flaws - city communication and long journey."
 },
 {
-    id: 3,
+    id: 5,
     img:"Iceland.jpg",
     destiny: "Iceland",
     date: "Unknown",
     description: "Still planning..."
 },
 {
-    id: 4,
+    id: 6,
     img:"utah_colorado_wyoming.jpg",
     destiny: "Utah/Kolorado/Wyoming",
     date: "Unknown",
